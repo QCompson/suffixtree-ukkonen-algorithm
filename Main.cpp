513aa36a79d80cf08f8f7ec4467330ac8bfd2fb9
KTomislav <tomislav.kres@gmail.com>
2015-01-14 21:26:43 +0100

    Edited version with some c++ code standard corrections

--- Main.cpp
+++ Main.cpp
@@ -1,49 +1,51 @@
 #include <iostream>
-#include <fstream>
-#include <string>
+#include <stdio.h>
+#include <map>
 #include <vector>
+#include <string>
 #include <sstream>
+#include <fstream>
+
 using namespace std;
+
 
 int currentPosition=-1;
 class Node;
-
-//An edge between two nodes is described by the index of the first character and the index of the last character
-//and represents a substring of the initial input
-class NormalEdge {
+class NormalEdge
+{
 public:
-    int startCharacterIndex;
+	int startCharacterIndex;
 	int endCharacterIndex;
-	Node *startNode;
+	Node *startNode; 
 	Node *endNode;
 
-    NormalEdge(){}
-
-    NormalEdge(int _startCharacterIndex, int _endCharacterIndex, Node *_startNode, Node *_endNode)
-	{
+	NormalEdge(int _startCharacterIndex, int _endCharacterIndex, Node *_startNode, Node *_endNode){
 		this->startCharacterIndex=_startCharacterIndex;
 		this->endCharacterIndex=_endCharacterIndex;
 		this->startNode=_startNode;
 		this->endNode=_endNode;
 	}
+	NormalEdge(){}
 
 	int returnEndCharacterIndex();
 };
 
 
-class Node {
+class Node
+{
 public:
-    int number;
 	Node *suffixEdge;
 	vector<NormalEdge> edges;
 	bool isLeaf;
-    Node() {}
-
-    Node(int _number, bool _isLeaf){
-        this->number = _number;
+	int number;
+	Node(int _number, bool _isLeaf){
 		this->isLeaf=_isLeaf;
-		this->suffixEdge=NULL;
-    }
+		this->number=_number;
+		suffixEdge=NULL;
+	}
+	Node(){}
+
+
 
 	void RemoveEdge(NormalEdge *e){
 		int index=-1;
@@ -55,7 +57,7 @@ public:
 		if(e->endNode!=NULL){
 			fe=e->endNode->number;
 		}
-		for(vector<NormalEdge>::iterator it = edges.begin(); it != edges.end(); ++it) {
+		for(vector<NormalEdge>::iterator it = edges.begin(); it != edges.end(); ++it){
 			NormalEdge *edgeTmp=&(*it);
 			int ss=0;
 			if(edgeTmp->startNode!=NULL){
@@ -82,71 +84,87 @@ int NormalEdge::returnEndCharacterIndex() {
 		}
 	}
 
-//The algorithm is based on the triple: Active Node, Active Edge and Active Lenght which keeps a better track
-//of the current step and helps out with characters already inserted in the tree, as well as with suffix links
-class Triple{
+class Triple
+{
 public:
 	Node *activeNode;
-	NormalEdge *activeEdge;
-	int length;
+    NormalEdge *activeEdge;
+    int length;
 
-	Triple() {}
-
-	Triple(Node * _activeNode, int _length){
-		this->activeNode=_activeNode;
-		this->length=_length;
-		this->activeEdge=NULL;
+	Triple(Node *_activeNode, int _length){
+		this->activeNode = _activeNode;
+		this->length = _length;
+		activeEdge=NULL;
 	}
+	Triple(){}
 };
 
-//The Tree instance which iscreated from the initial input.
-class SuffixTree {
-private:
-    Node *root;
-	int nodeNumber;
-	string inputString;
-	Triple *activePoint;
-	int remainder;
-	stringstream graphvizOutput;   //Output is in dot notation (gaphviz)
-	int startCharacterIndexForInsertInString;
-	Node *previousInsertedNode;
-
+class SuffixTree
+{
 public:
-    SuffixTree(string _inputString){
-        this->inputString = _inputString;
+	SuffixTree(string _inputString){
 		this->nodeNumber=1;
-		this->root=new Node(this->nodeNumber, false);
+		Node *rootNode=new Node(this->nodeNumber,false);
+		root=rootNode;
 		this->nodeNumber++;
+
+		this->inputString=_inputString;
 		this->remainder=0;
-		this->startCharacterIndexForInsertInString=0;
-		this->graphvizOutput.clear();
+		this->startCharacterIndexForInsertInString=0; //ovo možda ne treba? povezano s tekstom u nodeu
+		graphvizIspis.clear();
 		Triple *triple=new Triple(root, 0);
 		this->activePoint=triple;
-		this->previousInsertedNode=NULL;
-    }
+		previousInsertedNode=NULL;
+	}
 
-
-    void CreateSuffixTree(){
+	void CreateSuffixTree(){
 		int inputStringLen=inputString.length();
 		for(int i=0;i<inputStringLen; i++){
 			AddCharacter(inputString[i]);
 		}
 	}
 
+	string PrintTree(){
+		graphvizIspis.clear();
+        graphvizIspis<<"digraph { \n";
+        graphvizIspis<<"\t rankdir = LR; \n";
+        graphvizIspis<<"\t edge [arrowsize=0.4,fontsize=10] \n";
+        graphvizIspis<<"\t node1 [label=\"\",style=filled,fillcolor=lightgrey,shape=circle,width=.1,height=.1]; \n";
+        graphvizIspis<<"//------leaves------ \n";
+        PrintLeaves(root);
+        graphvizIspis<<"//------internal nodes------ \n";
+        PrintInternalNodes(root);
+        graphvizIspis<<"//------edges------ \n";
+        PrintEdges(root);
+        graphvizIspis<<"//------suffix links------ \n";
+	    PrintSuffixEdges(root);
+        graphvizIspis<<"} \n";
 
-    //Handling a new character while following all the rules
+        return graphvizIspis.str();
+	}
+
+private:
+	Node *root;
+	int nodeNumber;
+	string inputString;
+    Triple *activePoint;
+    int remainder;
+    int startCharacterIndexForInsertInString; //index of character in string from which new edge starts
+    stringstream graphvizIspis;
+	Node *previousInsertedNode; //node previously inserted during same step
+
 	void AddCharacter(char c){
-		this->previousInsertedNode=NULL;
+		previousInsertedNode=NULL;
 		currentPosition++;
-		this->remainder++;
+		remainder++;
 
-		while(this->remainder>0){
+		while(remainder>0){
 
-			if(this->activePoint->length==0){
-				this->startCharacterIndexForInsertInString=currentPosition;
+			if(activePoint->length==0){
+				startCharacterIndexForInsertInString=currentPosition;
 			}
-
 			if(NeedToInsertNewEdge(false)){
+
 				AddNewEdge(activePoint->activeNode, startCharacterIndexForInsertInString, startCharacterIndexForInsertInString);
 				if(previousInsertedNode!=NULL){
 					previousInsertedNode->suffixEdge=activePoint->activeNode;
@@ -155,9 +173,8 @@ public:
 			}
 			else{
 				activePoint->activeEdge=ChooseActiveEdge();
-				int edgeLen=activePoint->activeEdge->endCharacterIndex-activePoint->activeEdge->startCharacterIndex+1;
+				int edgeLen=activePoint->activeEdge->returnEndCharacterIndex()-activePoint->activeEdge->startCharacterIndex+1;
 				if(activePoint->length>=edgeLen){
-
 					startCharacterIndexForInsertInString+=edgeLen;
 					activePoint->length-=edgeLen;
 					activePoint->activeNode=activePoint->activeEdge->endNode;
@@ -165,7 +182,6 @@ public:
 					continue;
 				}
 				if(!NeedToInsertNewEdge(true)){
-
 					activePoint->length++;
 					if(previousInsertedNode!=NULL){
 						previousInsertedNode->suffixEdge=activePoint->activeNode;
@@ -174,13 +190,14 @@ public:
 					break;
 				}
 
-			    int splitIndex=activePoint->length+activePoint->activeEdge->startCharacterIndex;
+				int splitIndex=activePoint->length+activePoint->activeEdge->startCharacterIndex;
 				AddNewInternalEdge(splitIndex, currentPosition);
 				if(previousInsertedNode!=NULL){
 					previousInsertedNode->suffixEdge=activePoint->activeEdge->endNode;
 				}
 				previousInsertedNode=activePoint->activeEdge->endNode;
 			}
+
 			remainder--;
 			if(activePoint->activeNode->number==root->number && activePoint->length>0){
 				activePoint->length--;
@@ -196,10 +213,8 @@ public:
 				}
 			}
 		}
-
 	}
 
-    //Creating a new edge in the tree
 	void AddNewEdge(Node *startNode, int startIndex, int endIndex){
 			Node *leaf=new Node(nodeNumber, true);
 			nodeNumber++;
@@ -212,12 +227,14 @@ public:
 		nodeNumber++;
 
 		NormalEdge *splittedEdge=new NormalEdge(activePoint->activeEdge->startCharacterIndex, startIndex-1, activePoint->activeNode, splittedEdgeNode);
+
 		activePoint->activeNode->edges.push_back(*splittedEdge);
 		activePoint->activeEdge=ChooseActiveEdge();
 
 		Node *leafEdgeNode=new Node(nodeNumber, true);
 		nodeNumber++;
 		NormalEdge *leafEdge=new NormalEdge(endIndex, endIndex, splittedEdgeNode, leafEdgeNode);
+
 		splittedEdgeNode->edges.push_back(*leafEdge);
 
 		activePoint->activeEdge->startCharacterIndex+=activePoint->length;
@@ -229,16 +246,15 @@ public:
 		activePoint->activeEdge=splittedEdge;
 	}
 
+
 	bool NeedToInsertNewEdge(bool useActiveLen){
 		int len=1;
 		if(useActiveLen){
 			len+=activePoint->length;
 		}
 		if(activePoint->activeEdge==NULL){
-
 			int edgesSize=activePoint->activeNode->edges.size();
 			for(int i=0;i<edgesSize;i++){
-
 				if(len==1){
 					NormalEdge *tmp=&activePoint->activeNode->edges.at(i);
 					char s1=inputString.at(tmp->startCharacterIndex);
@@ -258,7 +274,7 @@ public:
 			}
 			return true;
 		}
-		else{	
+		else{
 			if(len==1){
 					char s1=inputString.at(activePoint->activeEdge->startCharacterIndex);
 					char s2=inputString.at(startCharacterIndexForInsertInString);
@@ -283,50 +299,32 @@ public:
 			NormalEdge *tmpEdge=&activePoint->activeNode->edges.at(i);
 			char s1=inputString.at(tmpEdge->startCharacterIndex);
 			char s2=inputString.at(startCharacterIndexForInsertInString);
-			if(s1==s2)//equals
-			{
+			if(s1==s2){//equals
 				return tmpEdge;
 			}
 		}
 		return NULL;
 	}
 
-	//Printing the edges in a standard .dot notation
 	void PrintEdges(Node *n){
 		int edgesSize=n->edges.size();
 		for(int i=0;i<edgesSize;i++){
 			NormalEdge *tmp=&n->edges.at(i);
-			graphvizOutput<<"\t node"<<n->number;
-			graphvizOutput<<" -> node"<<tmp->endNode->number;
-			graphvizOutput<<" [label=\"";
-			graphvizOutput<<inputString.substr(tmp->startCharacterIndex, tmp->returnEndCharacterIndex()-tmp->startCharacterIndex+1);
-			graphvizOutput<<"\",weight=3] \n";
+			graphvizIspis<<"\t node"<<n->number;
+			graphvizIspis<<" -> node"<<tmp->endNode->number;
+			graphvizIspis<<" [label=\"";
+			graphvizIspis<<inputString.substr(tmp->startCharacterIndex, tmp->returnEndCharacterIndex()-tmp->startCharacterIndex+1);
+			graphvizIspis<<"\",weight=3] \n";
 			PrintEdges(tmp->endNode);
-		}
-	}
-
-	void PrintInternalNodes(Node *n){
-		int edgesSize=n->edges.size();
-		if(n->number!=root->number && edgesSize>0){
-			graphvizOutput<<"\t node";
-			graphvizOutput<<n->number;
-			graphvizOutput<<" [label=\"\",style=filled,fillcolor=lightgrey,shape=circle,width=.07,height=.07] \n";
-		}
-		if(edgesSize>0){
-			for(int i=0;i<edgesSize;i++){
-				NormalEdge *e=&n->edges.at(i);
-				if(e->endNode!=NULL){
-					PrintInternalNodes(e->endNode);
-				}
-			}
 		}
 	}
 
 	void PrintLeaves(Node *n){
 		int edgesSize=n->edges.size();
 		if(edgesSize==0){
-			graphvizOutput<<"\t node"<<n->number;
-			graphvizOutput<<" [label=\"\",shape=point] \n";
+			cout<<n->number<<endl;
+			graphvizIspis<<"\t node"<<n->number;
+			graphvizIspis<<" [label=\"\",shape=point] \n";
 		}
 		else{
 			for(int i=0;i<edgesSize;i++){
@@ -338,11 +336,28 @@ public:
 		}
 	}
 
+	void PrintInternalNodes(Node *n){
+		int edgesSize=n->edges.size();
+		if(n->number!=root->number && edgesSize>0){
+			graphvizIspis<<"\t node";
+			graphvizIspis<<n->number;
+			graphvizIspis<<" [label=\"\",style=filled,fillcolor=lightgrey,shape=circle,width=.07,height=.07] \n";
+		}
+		if(edgesSize>0){
+			for(int i=0;i<edgesSize;i++){
+				NormalEdge *e=&n->edges.at(i);
+				if(e->endNode!=NULL){
+					PrintInternalNodes(e->endNode);
+				}
+			}
+		}
+	}
+
 	void PrintSuffixEdges(Node *n){
 		if(n->suffixEdge!=NULL){
-			graphvizOutput<<"\t node" ;
-			graphvizOutput<<n->number<<" -> node";
-			graphvizOutput<<n->suffixEdge->number<<" [label=\"\",weight=1,style=dotted] \n";
+			graphvizIspis<<"\t node" ;
+			graphvizIspis<<n->number<<" -> node";
+			graphvizIspis<<n->suffixEdge->number<<" [label=\"\",weight=1,style=dotted] \n";
 		}
 		int edgesSize=n->edges.size();
 		for(int i=0;i<edgesSize;i++){
@@ -353,44 +368,18 @@ public:
 		}
 	}
 
-	//Printing the tree in a .dot format which is a standard for graphviz visualization
-	string PrintTree()
-	{
-		graphvizOutput.clear();
-        graphvizOutput<<"digraph { \n";
-        graphvizOutput<<"\t rankdir = LR; \n";
-        graphvizOutput<<"\t edge [arrowsize=0.5,fontsize=12] \n";
-        graphvizOutput<<"\t node1 [label=\"\",style=filled,fillcolor=lightgrey,shape=circle,width=.1,height=.1]; \n";
-        graphvizOutput<<"//------leaves------ \n";
-        PrintLeaves(root);
-        graphvizOutput<<"//------nodes------ \n";
-        PrintInternalNodes(root);
-        graphvizOutput<<"//------edges------ \n";
-        PrintEdges(root);
-        graphvizOutput<<"//------suffix links------ \n";
-	    PrintSuffixEdges(root);
-        graphvizOutput<<"} \n";
-
-        return graphvizOutput.str();
-	}
-
 };
 
-//Methods handling input/output
-
-void WriteToFile(string str, char* fileName)
-{
-	ofstream outFile;
-    outFile.open(fileName);
-    outFile<<str;
-    outFile.close();
+void WriteToFile(string str){
+	ofstream myfile;
+    myfile.open ("tree.dot");
+    myfile << str;
+    myfile.close();
 }
 
-string ReadFromFile(const char *filename)
-{
+string ReadFromFile(const char *filename){
   ifstream inFile(filename);
-  if (inFile)
-  {
+  if (inFile){
     std::string input;
     inFile.seekg(0, std::ios::end);
     input.resize(inFile.tellg());
@@ -401,7 +390,19 @@ string ReadFromFile(const char *filename)
   }
 }
 
+int main(int argc, char* argv[]) {
 
-int main (int argc, char* argv[]) {
+	SuffixTree *st=new SuffixTree(ReadFromFile("inputString.txt"));
+	//SuffixTree *st=new SuffixTree("mississippi#");
+	st->CreateSuffixTree();
+	string result=st->PrintTree();
+	WriteToFile(result);
+
+	//If user wants to see the graphviz output
+	//cout <<result<<endl;
+
+
+	cout<<"The sufix tree creation is done"<<endl;
+
     return 0;
 }
